95f973a757e8c9fdb519a415f6ccdd964ffa70d8
Ajbar Imad <ajbar.imad.99@gmail.com>
2022-04-08 01:07:27 +0000

    Last Version 🚀

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import Video from './components/Video'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import Video from './components/Video';
+import db from  './Firebase'
+import './App.css';
 
 function App() {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        db.collection("posts").onSnapshot((snapshot) =>
+          setVideos(snapshot.docs.map((doc) => doc.data()))
+        );
+      }, []);
+
     return (
         <div className="app">
            <div className='app__videos'>
-            <Video 
-                url="https://v16-webapp.tiktok.com/defdaa2510155ff0fe7ee7f31db25ba1/624f6f82/video/tos/alisg/tos-alisg-pve-0037c001/1cf8aa634fd147c6ab97594471a4575f/?a=1988&br=2108&bt=1054&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=eXd.6HpoMyq8ZIRBGwe2NJlwyl7Gb&l=202204071710060102230830121E4A8627&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3dtNGc6ZnA1OTMzODczNEApZWU5aTU1ZmRlNzVnaTw8NWdeaDNlcjRvazNgLS1kMS1zcy0uL2ItNWMvYy9jYGAwLy46Yw%3D%3D&vl=&vr="
-                channel="AjbarImad"
-                description="Wow this works...!"
-                song="Islam-Sobhi_Kuraan"
-                likes={499}
-                messages={399}
-                shares={123}
-            />
-            <Video />
+               {videos.map( 
+                   ( {url,channel,description,song,likes,messages,shares}, index ) => (
+
+                       <Video key={index}
+                           url={url}
+                           channel={channel}
+                           description={description}
+                           song={song}
+                           likes={likes}
+                           messages={messages}
+                           shares={shares}
+                       />
+                   )
+                )}
            </div>
         </div>
     );
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,15 +1,18 @@
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAmkawrq3UxUS7m8N3ioQQtJsB7wvb6b8w",
-    authDomain: "tic-tok-96018.firebaseapp.com",
-    projectId: "tic-tok-96018",
-    storageBucket: "tic-tok-96018.appspot.com",
-    messagingSenderId: "368795081776",
-    appId: "1:368795081776:web:e1a2fa723c999e02d261f5",
-    measurementId: "G-YS4ZZ2WWSB"
-  };
+  apiKey: "AIzaSyAmkawrq3UxUS7m8N3ioQQtJsB7wvb6b8w",
+  authDomain: "tic-tok-96018.firebaseapp.com",
+  projectId: "tic-tok-96018",
+  storageBucket: "tic-tok-96018.appspot.com",
+  messagingSenderId: "368795081776",
+  appId: "1:368795081776:web:e1a2fa723c999e02d261f5",
+  measurementId: "G-YS4ZZ2WWSB"
+};
 
-  const firebaseApp = initializeApp(firebaseConfig)
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
   
-  const db = getFireStore(firebaseApp);
+  const db = firebaseApp.firestore();
+
+  export default db;
